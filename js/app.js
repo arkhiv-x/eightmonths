d6025ea6fa5e8a0fe1ad232ae6fdc1a283d031ef
arkhiv-x <cassbeaumont91@gmail.com>
2026-02-07 02:39:59 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,104 @@
+/* FRASES */
+
+const frases=[
+"Disciplina vence desânimo.",
+"Nada muda se eu não mudar."
+];
+
+if(document.getElementById("frase")){
+
+let f=localStorage.frase||0;
+
+frase.innerText=frases[f];
+
+localStorage.frase=(+f+1)%2;
+
+}
+
+/* LOGIN */
+
+function login(){
+
+let u=document.getElementById("user").value;
+let p=document.getElementById("pass").value;
+
+if(u==="Vitória" && p==="1997"){
+
+localStorage.logado="1";
+window.location="inicio.html";
+
+}else{
+
+document.getElementById("erro").innerText="Dados incorretos";
+
+}
+
+}
+
+/* LOGOUT */
+
+function logout(){
+
+localStorage.removeItem("logado");
+window.location="index.html";
+
+}
+
+/* PROTEÇÃO */
+
+if(
+!window.location.href.includes("index.html") &&
+localStorage.logado!=="1"
+){
+window.location="index.html";
+}
+
+/* MENU */
+
+function openMenu(){
+menu.classList.add("open");
+overlay.classList.add("show");
+}
+
+function closeMenu(){
+menu.classList.remove("open");
+overlay.classList.remove("show");
+}
+
+/* CHECK */
+
+function saveCheck(){
+
+let arr=JSON.parse(localStorage.checks||"[]");
+
+arr.push({
+d:new Date().toLocaleDateString(),
+c1:c1.checked,
+c2:c2.checked,
+c3:c3.checked,
+c4:c4.checked
+});
+
+localStorage.checks=JSON.stringify(arr);
+
+alert("Salvo 💖");
+
+}
+
+/* CONTADOR */
+
+function update(){
+
+let start=new Date("2026-02-09");
+let now=new Date();
+
+let d=Math.floor((now-start)/86400000)+1;
+
+if(document.getElementById("contador")){
+contador.innerText="Dia "+d+" de 120";
+}
+
+}
+
+setInterval(update,1000);
+update();
